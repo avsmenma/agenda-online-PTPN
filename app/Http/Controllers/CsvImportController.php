f0dc350bc59ed18912df3c00a58462f6f7bf1356
arcadealley <ketsutol1@gmail.com>
2025-12-22 14:49:26 +0700

    menambahkan direktori storage/app/csv_imports/

--- app/Http/Controllers/CsvImportController.php
+++ app/Http/Controllers/CsvImportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Dokumen;
 use Carbon\Carbon;
 
@@ -24,6 +25,12 @@ class CsvImportController extends Controller
         ]);
 
         try {
+            // Ensure csv_imports directory exists
+            $directory = 'csv_imports';
+            if (!Storage::exists($directory)) {
+                Storage::makeDirectory($directory);
+            }
+
             $file = $request->file('csv_file');
             $filename = time() . '_' . $file->getClientOriginalName();
             $path = $file->storeAs('csv_imports', $filename);
