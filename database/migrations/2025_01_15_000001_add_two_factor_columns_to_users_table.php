9e0b36fffb85c945682ec6e3efd2639d19e6463a
Mas Febri Adithya <ketsutol1@gmail.com>
2025-12-31 04:26:35 +0000

    mebgubah tipe kolom menjadi text pada two_factor_recovery_codes

--- database/migrations/2025_01_15_000001_add_two_factor_columns_to_users_table.php
+++ database/migrations/2025_01_15_000001_add_two_factor_columns_to_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->boolean('two_factor_enabled')->default(false)->after('remember_token');
             $table->text('two_factor_secret')->nullable()->after('two_factor_enabled');
             $table->timestamp('two_factor_confirmed_at')->nullable()->after('two_factor_secret');
-            $table->json('two_factor_recovery_codes')->nullable()->after('two_factor_confirmed_at');
+            $table->text('two_factor_recovery_codes')->nullable()->after('two_factor_confirmed_at');
         });
     }
 
