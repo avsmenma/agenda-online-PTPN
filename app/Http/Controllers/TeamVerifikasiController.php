c10b8b3b2f0f8da96f28b96b0de6ce6583a6a3c7
arcadealley <ketsutol1@gmail.com>
2026-08-03 05:42:59 +0700

    add query to get data nomor_kontrak

--- app/Http/Controllers/TeamVerifikasiController.php
+++ app/Http/Controllers/TeamVerifikasiController.php
@@ -220,6 +220,7 @@ class TeamVerifikasiController extends Controller
                 'dokumens.tanggal_spk',
                 'dokumens.tanggal_berakhir_spk',
                 'dokumens.no_spk',
+                'dokumens.nomor_kontrak',
                 'dokumens.nomor_miro',
                 'dokumens.nama_pengirim',
                 'dokumens.jenis_pembayaran',
